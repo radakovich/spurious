84803be32554cf7e7873c0946ec09100ce773b81
JJJaquette <jeanjacquesjaquette@gmail.com>
2014-01-31 22:52:49 -0600

    added reading a config file as well as having options to set the filename and path

--- spurious.js
+++ spurious.js
@@ -2,9 +2,23 @@ var fs = require('fs'),
     path = require('path');
 
 var spurious = module.exports = function(opt){
+    var self = this;
 
+    if(opt.configFile){
+        this.configFile = opt.configFile;
+    }
+
+    if(opt.configPath){
+        this.configPath = opt.configPath;
+    }
 };
 
 spurious.prototype = {
+    configFile: 'spurious.config.json',
+    configPath: '',
 
+    readFile: function(){
+        return JSON.parse(fs.readFileSync(this.configPath + this.configFile, 'utf8')); 
+    }
 };
+
--- test/spurious.js
+++ test/spurious.js
@@ -1,7 +1,14 @@
 describe("Spurious", function(){
-    var spurious = require('../spurious.js');
+    var Spurious = require('../spurious.js');
 
     it('should ', function(){
+        var spurious = new Spurious({
+            configFile: 'test.spurious.config.json',
+            configPath: 'test/'
+        });
 
+        var config = spurious.readFile();
+
+        expect(config.entities.length).toEqual(1);
     });
 });
